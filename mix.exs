e2ddfe544b3fc9759b894259cb637c12d0cc84d4
Vladimir Tsymbal <vladimirts1991@gmail.com>
2017-12-07 10:31:38 +0500

    update edeliver

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Myproject.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:edeliver, "~> 1.4.3"},
+      {:edeliver, "~> 1.4.4"},
       {:distillery, "~> 1.4"}
     ]
   end
